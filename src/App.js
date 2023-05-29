f9f2dbb4260c9845327fd9c45c42cdd23dfc1b35
Okpola Ugbolochi .S <88292390+linto94@users.noreply.github.com>
2023-05-29 17:01:19 +0100

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import "./App.css";
+import Footer from "./components/footer/Footer";
+import Header from "./components/header/Header";
+import Hero from "./components/hero/Hero";
+import useLocalStorage from "use-local-storage";
 
 function App() {
+  const [theme, setTheme] = useLocalStorage("theme", "light");
+  const [switchBtn, setSwitchBtn] = useState(false);
+
+  const toggleTheme = () => {
+    const newTheme = theme === "light" ? "dark" : "light";
+    setTheme(newTheme);
+    setSwitchBtn(!switchBtn);
+  };
+
+  useEffect(() => {
+    if (theme === "dark") {
+      setSwitchBtn(true);
+    }
+  }, [theme]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main">
+      <Header
+        myTheme={theme}
+        onToggleTheme={toggleTheme}
+        onSwitch={switchBtn}
+      />
+      <Hero myTheme={theme} />
+      <Footer myTheme={theme} />
     </div>
   );
 }
